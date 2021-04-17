5030e41996ecad7a91d747493abce2a65492889f
sydney <sydneychase5841@gmail.com>
2021-04-16 20:05:15 -0400

    Assignment Update

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
-function fetchGitHubByValue(value) {
-  fetch(`https://api.github.com/search/users?q=octocat${value}`, {
+function fetchGithubByValue(value) {
+  fetch(`https://api.github.com/search/users?q=${value}`, {
     headers: {
       "Accept": "application/vnd.github.v3+json"
     }
-  })
-  .then(resp => resp.json())
-  .then(json => console.log(json))
 
+  })
+  .then(res => res.json())
+  .then(json => console.log(json))
 }
+const submitBtn = document.getElementById("submit-button")
+const searchBox = document.getElementById('search')
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  fetchGithubByValue(searchBox.value)
+})
